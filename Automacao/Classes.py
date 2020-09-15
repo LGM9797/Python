42520948039310891799d0b4a465a3b9845c5b35
Lucas Monteiro <lucas.monteiro@kontrow.com.br>
2020-09-15 10:56:40 -0300

    Hebraica report

--- Automacao/Classes.py
+++ Automacao/Classes.py
@@ -1,4 +1,4 @@
-class LimpadorNome:
+'''class LimpadorNome:
     def __init__(self, valor, path):
         if(self.valorValido(valor)):
             self.valor = valor
@@ -7,9 +7,10 @@ class LimpadorNome:
             raise LookupError('Valor inválido')
 
     def ExtraiArgumentos(self):
-        underScore = self.valor.find('2020') + len('2020') #Achando o limite da string que quero remover
-        newName = self.path + '/' + self.valor[:underScore] #tirando o slice que preciso passar de volta
+        underScore = self.valor.find('2020',2) + len('2020') #Achando o limite da string que quero remover
+        newName = self.path +r"/" + self.valor[:underScore]+'.xlsx' #tirando o slice que preciso passar de volta
         return newName
+
 
     @staticmethod
     def valorValido(valor):
@@ -17,3 +18,42 @@ class LimpadorNome:
             return True
         else:
             return False
+        '''
+
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'fizzBuzz' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def fizzBuzz(n):
+    # Write your code here
+
+    x = range(1,n+1)
+    for i in x:
+        if (i % 3 == 0 and i % 5 == 0 and i!=0):
+            print('FizzBuzz')
+        elif (i % 3 == 0 and i % 5 != 0):
+            print('Fizz')
+        elif (i % 5 == 0 and i % 3 != 0):
+            print('Buzz')
+        else:
+            print(i)
+
+# n = int(input().strip())
+n = 15
+fizzBuzz(n)
+#
+# if __name__ == '__main__':
+#     n = int(input().strip())
+#
+#     fizzBuzz(n)
--- Automacao/Main.py
+++ Automacao/Main.py
@@ -1,18 +1,23 @@
 from Classes import LimpadorNome
 import os
 
-path = '/Users/lucasmonteiro/Desktop/GP1/'
-print(os.listdir(path))
+path = r"C:\Users\Lucas Monteiro\Desktop\GP-3/"
+path2 = r"C:\Users\Lucas Monteiro\Desktop\GP-1/"
 
 for arquivos in os.listdir(path):
-    if(arquivos != '.DS'):
-        oldName = arquivos
-        nome1 = (LimpadorNome(arquivos, path))
-        nome1 = nome1.ExtraiArgumentos()
-        oldName1 = path+oldName
-        os.rename(oldName1, nome1)
+    #if(arquivos != '.DS'):
+    oldName = arquivos
+    nome1 = (LimpadorNome(arquivos,path))
+    nome1 = nome1.ExtraiArgumentos()
+    oldName1 = path+oldName
+    os.rename(oldName1, nome1)
 
-
-
-
-
+for arquivos in os.listdir(path2):
+    #if(arquivos != '.DS'):
+    oldName = arquivos
+    nome1 = (LimpadorNome(arquivos,path2))
+    nome1 = nome1.ExtraiArgumentos()
+    oldName1 = path2+oldName
+    print(oldName1)
+    os.rename(oldName1, nome1)
+    print(nome1)
